83a7e5f72e38310e8d2d3aa4496320f75d4e70a1
Richard Prillwitz <richard.prillwitz@myracloud.com>
2018-04-25 16:48:35 +0200

    more initial code

--- acpi/acpi.go
+++ acpi/acpi.go
@@ -1,8 +1,66 @@
 package acpi
 
-import "os/exec"
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
+)
 
 var path string
+
+//
+// ACPI stores the parsed output of acpi
+//
+type ACPI struct {
+	BatteryInformation []*BatteryInformation
+	AdapterInformation []*AdapterInformation
+	ThermalInformation []*ThermalInformation
+	CoolingInformation []*CoolingInformation
+}
+
+//
+// BatteryInformation stores parsed information for a battery
+//
+type BatteryInformation struct {
+	Number                  int
+	Status                  string
+	Level                   int
+	DesignCapacity          int
+	LastFullCapacity        int
+	LastFullCapacityPercent int
+}
+
+//
+// AdapterInformation stores parsed information for an adapter
+//
+type AdapterInformation struct {
+	Number int
+	Status string
+}
+
+//
+// ThermalInformation stores parsed thermal information
+//
+type ThermalInformation struct {
+	Number            int
+	Status            string
+	Degree            float32
+	Unit              string
+	CriticalTripPoint float32
+}
+
+//
+// CoolingInformation stores parsed information for cooling
+//
+type CoolingInformation struct {
+	Number       int
+	Processor    int
+	ProcessorMax int
+	// add state information
+}
 
 func init() {
 	var err error
@@ -66,4 +124,99 @@ func Details() ([]byte, error) {
 //
 func Raw(args ...string) ([]byte, error) {
 	return exec.Command(path, args...).Output()
+}
+
+//
+// Parse converts the given raw acpi output to an ACPI struct
+//
+func Parse(raw []byte) (*ACPI, error) {
+	lines := bytes.Split(raw[:len(raw)-1], []byte("\n"))
+
+	info := &ACPI{}
+
+	var tmpMap = make(map[string]map[int][][]byte)
+
+	for _, l := range lines {
+		kv := bytes.Split(l, []byte(":"))
+
+		if len(tmpMap[string(kv[0][:7])]) <= 0 {
+			tmpMap[string(kv[0][:7])] = make(map[int][][]byte)
+		}
+
+		number, err := strconv.Atoi(strings.Trim(string(kv[0][7:]), " "))
+		if err != nil {
+			fmt.Println(err)
+		}
+		tmpMap[string(kv[0][:7])][number] = append(tmpMap[string(kv[0][:7])][0], kv[1])
+	}
+
+	for k, v := range tmpMap {
+		switch k {
+		case "Battery":
+			batteryInformation, err := parseBattery(v)
+			if err != nil {
+				continue
+			}
+			info.BatteryInformation = batteryInformation
+			break
+		case "Adapter":
+			adapterInformation, err := parseAdapter(v)
+			if err != nil {
+				continue
+			}
+			info.AdapterInformation = adapterInformation
+			break
+		case "Thermal":
+			thermalInformation, err := parseThermal(v)
+			if err != nil {
+				continue
+			}
+			info.ThermalInformation = thermalInformation
+			break
+		case "Cooling":
+			coolingInformation, err := parseCooling(v)
+			if err != nil {
+				continue
+			}
+			info.CoolingInformation = coolingInformation
+			break
+		default:
+			log.Println("unknown key " + k)
+			break
+		}
+	}
+	fmt.Println(info)
+	return info, nil
+}
+
+//
+// parseBattery ...
+//
+func parseBattery(raw map[int][][]byte) (information []*BatteryInformation, err error) {
+	//todo
+	return
+}
+
+//
+// parseAdapter ..
+//
+func parseAdapter(raw map[int][][]byte) (information []*AdapterInformation, err error) {
+	//todo
+	return
+}
+
+//
+// parseThermal ...
+//
+func parseThermal(raw map[int][][]byte) (information []*ThermalInformation, err error) {
+	//todo
+	return
+}
+
+//
+// parseCooling ...
+//
+func parseCooling(raw map[int][][]byte) (information []*CoolingInformation, err error) {
+	//todo
+	return
 }
